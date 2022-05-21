61d135072492deab75698f96e29aa0b1a900fce6
AdrienHorgnies <adrien.pierre.horgnies@gmail.com>
2022-05-21 12:01:27 +0200

    remove useless lock_time

--- db/models.py
+++ db/models.py
@@ -4,6 +4,9 @@ import pendulum
 
 
 class Block(models.Model):
+    """
+    Represents a Bitcoin block, containing only the fields useful for the simulation purpose.
+    """
     hash = models.CharField(max_length=64, db_index=True)
     height = models.IntegerField(unique=True)
     time = models.DateTimeField(db_index=True)
@@ -11,6 +14,11 @@ class Block(models.Model):
 
     @classmethod
     def save_from_api(cls, api_block: ApiBlock):
+        """
+        Create and save a block from the API data.
+
+        :param api_block: the block from the API
+        """
         db_block = Block()
         db_block.hash = api_block['hash']
         db_block.time = pendulum.from_timestamp(api_block['time']).naive()
@@ -30,17 +38,25 @@ class Block(models.Model):
 
 
 class Transaction(models.Model):
+    """
+    Represents a Bitcoin transaction, containing only the fields useful for the simulation purpose.
+    """
     hash = models.CharField(max_length=64, db_index=True)
-    lock_time = models.DateTimeField()
     weight = models.IntegerField()
     fee = models.IntegerField()
     block = models.ForeignKey(Block, related_name='transactions', on_delete=models.CASCADE)
 
     @classmethod
     def to_unsaved_db_tx(cls, api_tx: ApiTransaction, block: Block):
+        """
+        Create a Transaction from API data, without saving it in the database.
+
+        :param api_tx: The data from the API
+        :param block: The block that contains the transaction
+        :return: the transaction
+        """
         db_tx = Transaction()
         db_tx.hash = api_tx['hash']
-        db_tx.lock_time = pendulum.from_timestamp(api_tx['lock_time']).naive()
         db_tx.weight = api_tx['weight']
         db_tx.fee = api_tx['fee']
         db_tx.block = block
@@ -49,6 +65,5 @@ class Transaction(models.Model):
     def __str__(self):
         return f'<{Transaction.__name__} ' \
                f'hash="{self.hash[-8:]}" ' \
-               f'lock_time={str(self.lock_time)} ' \
                f'weight={str(self.weight)} ' \
                f'block="{self.block.hash[-8:]}">'
